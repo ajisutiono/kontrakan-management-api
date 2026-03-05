63ebc22f72d85a6ea74fc6663d91ea6a02a8d83f
Aji Sutiono <raisadajiwa@gmail.com>
2026-03-05 16:46:57 +0700

    create table users and authentications

---
+++ migrations/1772702623069_create-table-users.js
@@ -0,0 +1,56 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+    pgm.createTable('users', {
+        id: {
+            type: 'UUID',
+            primaryKey: true,
+            default: pgm.func('gen_random_uuid()'),
+        },
+        name: {
+            type: 'VARCHAR(100)',
+            notNull: true,
+        },
+        email: {
+            type: 'VARCHAR(100)',
+            notNull: true,
+            unique: true,
+        },
+        password: {
+            type: 'TEXT',
+            notNull: true,
+        },
+        role: {
+            type: 'VARCHAR',
+            notNull: true,
+            // note: 'owner' | 'tenant',
+        },
+        created_at: {
+            type: 'TIMESTAMP',
+            notNull: true,
+            default: pgm.func('now()'),
+        },
+        updated_at: {
+            type: 'TIMESTAMP',
+            notNull: true,
+            default: pgm.func('now()'),
+        },
+    });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+    pgm.dropTable('users');
+};
---
+++ migrations/1772702746010_create-table-authentications.js
@@ -0,0 +1,44 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+    pgm.createTable('authentications', {
+        id: {
+            type: 'UUID',
+            primaryKey: true,
+            default: pgm.func('gen_random_uuid()'),
+        },
+        token: {
+            type: 'TEXT',
+            notNull: true,
+            unique: true,
+        },
+        user_id: {
+            type: 'UUID',
+            notNull: true,
+            references: '"users"',
+            onDelete: 'CASCADE',
+        },
+        created_at: {
+            type: 'TIMESTAMP',
+            notNull: true,
+            default: pgm.func('now()'),
+        },
+    });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+    pgm.dropTable('authentications');
+};

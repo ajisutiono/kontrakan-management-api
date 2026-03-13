beb70ea30bf3ee15cbb80d2ee8ef7a42590e4fed
Aji Sutiono <raisadajiwa@gmail.com>
2026-03-13 22:45:24 +0700

    feat: add awilix dependency injection container

--- src/Infrastructures/container.js
+++ src/Infrastructures/container.js
@@ -3,11 +3,11 @@ import { createContainer, asValue, asClass, } from 'awilix'
 import { randomUUID } from 'crypto'
 import bcrypt from 'bcrypt'
 
-import pool from './database/postgres'
-import UserRepositoryPostgres from './repository/UserRepositoryPostgres'
-import BcryptPasswordHash from './security/BcryptPasswordHash'
-import RegexPasswordValidator from './security/RegexPasswordValidator'
-import AddUserUseCase from '../Applications/use_case/AddUserUseCase'
+import pool from './database/postgres/pool.js'
+import UserRepositoryPostgres from './repository/UserRepositoryPostgres.js'
+import BcryptPasswordHash from './security/BcryptPasswordHash.js'
+import RegexPasswordValidator from './security/RegexPasswordValidator.js'
+import AddUserUseCase from '../Applications/use_case/AddUserUseCase.js'
 
 const container = createContainer()
 
@@ -16,6 +16,7 @@ container.register({
   pool: asValue(pool),
   bcrypt: asValue(bcrypt),
   idGenerator: asValue(randomUUID),
+  saltRound: asValue(10),
 
   // Repositories
   userRepository: asClass(UserRepositoryPostgres).singleton(),

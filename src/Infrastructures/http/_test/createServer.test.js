e2750c4bfff3bb433730035a5a11bf26454440cf
Aji Sutiono <raisadajiwa@gmail.com>
2026-03-14 20:26:34 +0700

    test: add 500 error and inject container in createServer test

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -4,9 +4,10 @@ import request from 'supertest'
 import pool from '../../database/postgres/pool.js'
 import UsersTableTestHelper from '../../../../tests/UsersTableTestHelper.js'
 import createServer from '../createServer.js'
+import container from '../../container.js'
 
 describe('HTTP server', () => {
-  const server = createServer()
+  const server = createServer(container)
 
   afterAll(async() => {
     await pool.end()
@@ -15,6 +16,28 @@ describe('HTTP server', () => {
   afterEach(async() => {
     await UsersTableTestHelper.cleanTable()
   })
+
+  /*
+  Scenarios test 400:
+  1. REGISTER_USER.NOT_CONTAIN_NEEDED_PROPERTY ✅
+     message: tidak dapat membuat user baru karena properti yang dibutuhkan tidak ada
+  2. REGISTER_USER.NOT_MEET_DATA_TYPE_SPECIFICATION ✅
+     message: tidak dapat membuat user baru karena tipe data tidak sesuai
+  3. REGISTER_USER.NAME_TOO_LONG ✅
+     message: tidak dapat membuat user baru karena nama terlalu panjang
+  4.REGISTER_USER.EMAIL_TOO_LONG ✅
+     message: tidak dapat membuat user baru karena email terlalu panjang
+  5.REGISTER_USER.PASSWORD_BELOW_MINIMUM_LENGTH ✅
+     message: tidak dapat membuat user baru karena password terlalu pendek
+
+  6. REGISTER_USER.INVALID_EMAIL  ✅
+     message: tidak dapat membuat user baru karena format email tidak valid
+  7. REGISTER_USER.INVALID_ROLE ✅
+     message:tidak dapat membuat user baru karena role tidak valid
+  8. REGISTER_USER.NAME_CONTAIN_RESTRICTED_CHARACTER ✅
+     message: tidak dapat membuat user baru karena nama mengandung karakter terlarang
+
+  */
 
   describe('when POST /api/users', () => {
     it('should response 201 and persisted user', async() => {
@@ -56,13 +79,13 @@ describe('HTTP server', () => {
 
       expect(response.status).toBe(400)
       expect(response.body.status).toBe('fail')
+      expect(response.body.message).toBe('tidak dapat membuat user baru karena email sudah ada')
     })
 
-    it('should response 400 when payload invalid', async() => {
-
+    it('should response 400 when payload name contain restricted character', async() => {
       const requestPayload = {
-        name: 'Test User',
-        email: 'invalid-email',
+        name: 'Test@User', // symbol restricted
+        email: 'testing@example.com',
         password: 'Password1!',
         role: 'owner',
       }
@@ -73,6 +96,158 @@ describe('HTTP server', () => {
 
       expect(response.status).toBe(400)
       expect(response.body.status).toBe('fail')
+      expect(response.body.message).toBe('tidak dapat membuat user baru karena nama mengandung karakter terlarang')
+    })
+
+    it('should response 400 when payload email invalid', async() => {
+
+      const requestPayload = {
+        name: 'Test User',
+        email: 'invalid-email', // payload invalid
+        password: 'Password1!',
+        role: 'owner',
+      }
+
+      const response = await request(server)
+        .post('/api/users')
+        .send(requestPayload)
+
+      expect(response.status).toBe(400)
+      expect(response.body.status).toBe('fail')
+      expect(response.body.message).toBe('tidak dapat membuat user baru karena format email tidak valid')
+    })
+
+    it('should response 400 when payload not contain needed property', async() => {
+
+      const requestPayload = {
+        // missing name
+        email: 'testing@mail.com',
+        password: 'Password1!',
+        role: 'owner',
+      }
+
+      const response = await request(server)
+        .post('/api/users')
+        .send(requestPayload)
+      
+      expect(response.status).toBe(400)
+      expect(response.body.status).toBe('fail')
+      expect(response.body.message).toBe('tidak dapat membuat user baru karena properti yang dibutuhkan tidak ada')
+    })
+
+    it('should response 400 when payload not meet data type specification', async() => {
+
+      const requestPayload = {
+        name: 'Test User',
+        email: 'testing@mail.com',
+        password: 'Password1!',
+        role: true,
+      }
+
+      const response = await request(server)
+        .post('/api/users')
+        .send(requestPayload)
+      
+      expect(response.status).toBe(400)
+      expect(response.body.status).toBe('fail')
+      expect(response.body.message).toBe('tidak dapat membuat user baru karena tipe data tidak sesuai')
+    })
+
+    it('should response 400 when payload name too long', async() => {
+
+      const requestPayload = {
+        name: 'a'.repeat(101),
+        email: 'testing@mail.com',
+        password: 'Password1!',
+        role: 'owner',
+      }
+
+      const response = await request(server)
+        .post('/api/users')
+        .send(requestPayload)
+      
+      expect(response.status).toBe(400)
+      expect(response.body.status).toBe('fail')
+      expect(response.body.message).toBe('tidak dapat membuat user baru karena nama terlalu panjang')
+    })
+
+    it('should response 400 when payload email too long', async() => {
+
+      const requestPayload = {
+        name: 'Test User',
+        email: 'a'.repeat(101) + '@mail.com',
+        password: 'Password1!',
+        role: 'owner',
+      }
+
+      const response = await request(server)
+        .post('/api/users')
+        .send(requestPayload)
+      
+      expect(response.status).toBe(400)
+      expect(response.body.status).toBe('fail')
+      expect(response.body.message).toBe('tidak dapat membuat user baru karena email terlalu panjang')
+    })
+
+    it('should response 400 when payload password too short', async() => {
+
+      const requestPayload = {
+        name: 'Test User',
+        email: 'testing@mail.com',
+        password: 'Pass1!', // password < 8
+        role: 'owner',
+      }
+
+      const response = await request(server)
+        .post('/api/users')
+        .send(requestPayload)
+      
+      expect(response.status).toBe(400)
+      expect(response.body.status).toBe('fail')
+      expect(response.body.message).toBe('tidak dapat membuat user baru karena password terlalu pendek')
+    })
+
+    it('should response 400 when payload role not contain correctly', async() => {
+
+      const requestPayload = {
+        name: 'Test User',
+        email: 'testing@mail.com',
+        password: 'Password1!',
+        role: 'admin', // [onwer or tentant]
+      }
+
+      const response = await request(server)
+        .post('/api/users')
+        .send(requestPayload)
+      
+      expect(response.status).toBe(400)
+      expect(response.body.status).toBe('fail')
+      expect(response.body.message).toBe('tidak dapat membuat user baru karena role tidak valid')
+    })
+
+    it('should response 500 when unhandled error occurs', async () => {
+      const requestPayload = {
+        name: 'Test User',
+        email: 'testing@mail.com',
+        password: 'Password1!',
+        role: 'owner',
+      }
+
+      // Buat server baru dengan container yang di-mock
+      // supaya throw error tidak terduga
+      const errorServer = createServer({
+        resolve: () => {
+          throw new Error('Unexpected error')
+        },
+      })
+
+      const response = await request(errorServer)
+        .post('/api/users')
+        .send(requestPayload)
+
+      expect(response.status).toBe(500)
+      expect(response.body.status).toBe('error')
+      expect(response.body.message).toBe('Internal server error')
     })
   })
 })

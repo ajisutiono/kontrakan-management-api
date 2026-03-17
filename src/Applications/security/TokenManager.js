3267b16be7499de3eead2b37e7271cf605371b21
Aji Sutiono <raisadajiwa@gmail.com>
2026-03-17 16:39:22 +0700

    feat: create TokenManager abstract

---
+++ src/Applications/security/TokenManager.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+class TokenManager {
+  async createAccessToken(payload) {
+    throw new Error('TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED')
+  }
+
+  async createRefreshToken(payload) {
+    throw new Error('TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED')
+  }
+
+  async verifyRefreshToken(token) {
+    throw new Error('TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED')
+  }
+
+  async decodePayload(token) {
+    throw new Error('TOKEN_MANAGER.METHOD_NOT_IMPLEMENTED')
+  }
+}

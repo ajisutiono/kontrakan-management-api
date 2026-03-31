8e64bdd98523353aad6f71443600fe86bb942403
Aji Sutiono <raisadajiwa@gmail.com>
2026-03-31 21:00:19 +0700

    feat: create use case for refresh authentication

---
+++ src/Applications/use_case/RefreshAuthenticationUseCase.js
@@ -0,0 +1,23 @@
+import RefreshAuthentication from '../../Domains/authentications/entities/RefreshAuthentication.js'
+
+class RefreshAuthenticationUseCase {
+  constructor({
+    authenticationRepository,
+    tokenManager
+  }) {
+    this._authenticationRepository = authenticationRepository
+    this._tokenManager = tokenManager
+  }
+
+  async execute(useCasePayload) {
+    const { refreshToken } = new RefreshAuthentication(useCasePayload)
+
+    await this._authenticationRepository.checkAvailableToken(refreshToken)
+    
+    const { email, id }  = await this._tokenManager.verifyRefreshToken(refreshToken)
+
+    return this._tokenManager.createAccessToken({ email, id })
+  }
+}
+
+export default RefreshAuthenticationUseCase

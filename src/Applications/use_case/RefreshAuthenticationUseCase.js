7842fe64e9fbd4a1ad46e35be79b7183a1fe5726
Aji Sutiono <raisadajiwa@gmail.com>
2026-04-02 20:06:51 +0700

    refactor: change the logic order from checkAvailabilityToken to verifyRefreshToken first.

--- src/Applications/use_case/RefreshAuthenticationUseCase.js
+++ src/Applications/use_case/RefreshAuthenticationUseCase.js
@@ -11,11 +11,11 @@ class RefreshAuthenticationUseCase {
 
   async execute(useCasePayload) {
     const { refreshToken } = new RefreshAuthentication(useCasePayload)
-
-    await this._authenticationRepository.checkAvailabilityToken(refreshToken)
     
     await this._tokenManager.verifyRefreshToken(refreshToken)
 
+    await this._authenticationRepository.checkAvailabilityToken(refreshToken)
+    
     const { email, id } = await this._tokenManager.decodePayload(refreshToken)
 
     return this._tokenManager.createAccessToken({ email, id })

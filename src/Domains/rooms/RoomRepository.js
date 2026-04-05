58bfa1bdc5cda3e503828483d64a34ea3cac4c5b
Aji Sutiono <raisadajiwa@gmail.com>
2026-04-05 17:45:02 +0700

    feat: add AddRoom interface in RoomRepository

---
+++ src/Domains/rooms/RoomRepository.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+class RoomRepository {
+  async addRoom(room) {
+    throw new Error('ROOM_REPOSITORY.METHOD_NOT_IMPLEMENTED')
+  }
+}
+
+export default RoomRepository

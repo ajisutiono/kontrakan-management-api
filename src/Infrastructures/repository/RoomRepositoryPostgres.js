fa2579372d827726b8888a95383f8be1e3340052
Aji Sutiono <raisadajiwa@gmail.com>
2026-04-06 12:22:41 +0700

    feat: add RoomRepositoryPostgres for addRoom method implementation

---
+++ src/Infrastructures/repository/RoomRepositoryPostgres.js
@@ -0,0 +1,28 @@
+import { randomUUID } from 'crypto'
+
+import RoomRepository from '../../Domains/rooms/RoomRepository.js'
+
+class RoomRepositoryPostgres extends RoomRepository {
+  /* istanbul ignore next */
+  constructor({ pool, idGenerator = randomUUID }) {
+    super()
+    this._pool = pool
+    this._idGenerator = idGenerator
+
+  }
+
+  async addRoom(room) {
+    const { owner_id, room_number, type, price, facilities } = room
+    const id = this._idGenerator()
+
+    const query = {
+      text: 'INSERT INTO rooms (id, owner_id, room_number, type, price, facilities) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id, owner_id, room_number',
+      values: [id, owner_id, room_number, type, price, facilities]
+    }
+
+    const result = await this._pool.query(query)
+    return result.rows[0]
+  }
+}
+
+export default RoomRepositoryPostgres

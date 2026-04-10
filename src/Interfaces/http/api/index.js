270a7a5ccef26b4f3d87f0964ffbc888b750be53
Aji Sutiono <raisadajiwa@gmail.com>
2026-04-10 21:11:16 +0700

    feat: create room api for /rooms endpoint

--- src/Interfaces/http/api/index.js
+++ src/Interfaces/http/api/index.js
@@ -2,12 +2,14 @@ import { Router } from 'express'
 
 import createUsersApi from './users/index.js'
 import createAuthenticationsApi from './authentications/index.js'
+import createRoomApi from './rooms/index.js'
 
 const createRouter = (container) => {
   const router = Router()
 
   router.use('/users', createUsersApi(container))
   router.use('/authentications', createAuthenticationsApi(container))
+  router.use('/rooms', createRoomApi(container))
 
   return router
 }

bbd5123816934f687e77e4a5a5a886e2a99f48e4
Aji Sutiono <raisadajiwa@gmail.com>
2026-04-10 21:02:20 +0700

    refactor: add parameter payload role and create validation role equal owner

--- src/Applications/use_case/AddRoomUseCase.js
+++ src/Applications/use_case/AddRoomUseCase.js
@@ -1,20 +1,21 @@
 import RegisterRoom from '../../Domains/rooms/entities/RegisterRoom.js'
 import RegisteredRoom from '../../Domains/rooms/entities/RegisteredRoom.js'
+import AuthorizationError from '../../Commons/exceptions/AuthorizationError.js'
 
 class AddRoomUseCase {
-  constructor({
-    roomRepository
-  }) {
+  constructor({ roomRepository }) {
     this._roomRepository = roomRepository
   }
 
-  async execute(useCasePayload) {
+  async execute({ role, ...roomData }) {
+    if (role !== 'owner') {
+      throw new AuthorizationError('hanya owner yang dapat menambahkan kamar')
+    }
 
-    const registerRoom = new RegisterRoom(useCasePayload)
+    const registerRoom = new RegisterRoom(roomData)
     const registeredRoom = await this._roomRepository.addRoom(registerRoom)
 
     return new RegisteredRoom(registeredRoom)
-
   }
 }
 

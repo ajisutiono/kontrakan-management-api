40388422fac884abdc94689c8118212e46e3c295
Aji Sutiono <raisadajiwa@gmail.com>
2026-05-16 09:38:56 +0700

    feat(api): register /me/rooms endpoint

--- src/Interfaces/http/api/index.js
+++ src/Interfaces/http/api/index.js
@@ -3,6 +3,7 @@ import { Router } from 'express'
 import createUsersApi from './users/index.js'
 import createAuthenticationsApi from './authentications/index.js'
 import createRoomApi from './rooms/index.js'
+import createMyRoomApi from './me/rooms/index.js'
 
 const createRouter = (container) => {
   const router = Router()
@@ -10,6 +11,7 @@ const createRouter = (container) => {
   router.use('/users', createUsersApi(container))
   router.use('/authentications', createAuthenticationsApi(container))
   router.use('/rooms', createRoomApi(container))
+  router.use('/me/rooms', createMyRoomApi(container))
 
   return router
 }

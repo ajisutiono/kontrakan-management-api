b987e72dc85d0f6ea349cb9109db41d80b5df1ca
Aji Sutiono <raisadajiwa@gmail.com>
2026-05-16 09:42:23 +0700

    fix(my-rooms): return router instance from createMyRoomRouter

--- src/Interfaces/http/api/me/rooms/route.js
+++ src/Interfaces/http/api/me/rooms/route.js
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 
-import authMiddleware from '../../../middleware/authMiddleware'
+import authMiddleware from '../../../middleware/authMiddleware.js'
 
 const createMyRoomRouter = (controller, container) => {
   const router = Router()
 
   router.get('/', authMiddleware(container), controller.getMyRooms)
+
+  return router
 }
 
 export default createMyRoomRouter

496982d7ea13eda70e6f68382d64a7f35bb0f2c6
Aji Sutiono <raisadajiwa@gmail.com>
2026-07-01 11:48:40 +0700

    feat: add logic for updateRoomUseCase

---
+++ src/Applications/use_case/UpdateRoomUseCase.js
@@ -0,0 +1,26 @@
+import UpdateRoom from '../../Domains/rooms/entities/UpdateRoom.js'
+import AuthorizationError from '../../Commons/exceptions/AuthorizationError.js'
+
+class UpdateRoomUseCase {
+  constructor({ roomRepository }) {
+    this._roomRepository = roomRepository
+  }
+
+  async execute({ roomId, ownerId, role, payload }) {
+    if(role !== 'owner') {
+      throw new AuthorizationError('hanya owner yang dapat mengedit ruangan')
+    }
+
+    const updateRoom = new UpdateRoom(payload)
+
+    const existingRoom = await this._roomRepository.getRoomById(updateRoom)
+
+    if(existingRoom.owner_id !== ownerId) {
+      throw new AuthorizationError('Anda tidak memiliki akses untuk mengedit kamar ini')
+    }
+
+    return await this._roomRepository.updateRoomById(roomId, updateRoom)
+  }
+}
+
+export default UpdateRoomUseCase

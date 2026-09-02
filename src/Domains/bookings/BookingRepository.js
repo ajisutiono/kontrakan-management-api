932fe0fd476b5e6ad50b09727d33bf8032f3d1fc
Aji Sutiono <raisadajiwa@gmail.com>
2026-09-02 17:15:29 +0700

    feat: add BookingRepository abstract class

---
+++ src/Domains/bookings/BookingRepository.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+class BookingRepository {
+  async addBooking(booking) {
+    throw new Error('BOOKING_REPOSITORY.METHOD_NOT_IMPLEMENTED')
+  }
+}
+
+export default BookingRepository

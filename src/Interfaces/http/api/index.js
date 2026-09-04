28de95a755a11ec9737d37123fc7ea950954e782
Aji Sutiono <raisadajiwa@gmail.com>
2026-09-04 20:47:13 +0700

    feat: register bookings route to main router

--- src/Interfaces/http/api/index.js
+++ src/Interfaces/http/api/index.js
@@ -4,6 +4,7 @@ import createUsersApi from './users/index.js'
 import createAuthenticationsApi from './authentications/index.js'
 import createRoomApi from './rooms/index.js'
 import createMyRoomsApi from './me/rooms/index.js'
+import createBookingApi from './bookings/index.js'
 
 const createRouter = (container) => {
   const router = Router()
@@ -12,6 +13,7 @@ const createRouter = (container) => {
   router.use('/authentications', createAuthenticationsApi(container))
   router.use('/rooms', createRoomApi(container))
   router.use('/me/rooms', createMyRoomsApi(container))
+  router.use('/bookings', createBookingApi(container))
 
   return router
 }
